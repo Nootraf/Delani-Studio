3ddc1c3d37f9976084971a856421b39fd67cc972
Fartoon Abdi <twitwi457@gmail.com>
2020-03-05 11:06:58 +0300

    add all the recent pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-    $("#design").click(function() {
-        $("#design-icon").toggle();
-        $("#design").toggle();
+
+    $(".btn_1").click(function() {
+        $(".par_1").show();
+        $(".btn_1").hide();
     });
-    $("#dev").click(function() {
-        $(".dev-icon").toggle();
-        $(".dev").toggle();
+    $("p.par_1").click(function() {
+        $(".par_1").hide();
+        $(".btn_1").show();
     });
-    $("#pm").click(function() {
-        $(".pm-icon").toggle();
-        $(".product").toggle();
+    $(".btn_2").click(function() {
+        $(".par_2").show();
+        $(".btn_2").hide();
+    });
+    $(".par_2").click(function() {
+        $(".par_2").hide();
+        $(".btn_2").show();
+    });
+    $(".btn_3").click(function() {
+        $(".par_3").show();
+        $(".btn_3").hide();
+    });
+    $(".par_3").click(function() {
+        $(".btn_3").show();
+        $(".par_3").hide();
     });
 });
